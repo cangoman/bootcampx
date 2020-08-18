991292c41d559ada3b72585e1157e516722f3fdc
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-18 14:41:48 +0000

    completed 4_queries activities

---
+++ 4_queries/teachers_number_assistances.sql
@@ -0,0 +1,13 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests) as total_assistances
+FROM assistance_requests
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+JOIN teachers ON teacher_id = teachers.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY cohort, teacher
+ORDER BY teacher;
+
+
+
+
+

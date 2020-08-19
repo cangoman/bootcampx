635c9433e8286b00d6ed6553f0033f57f7b4a58d
Camilo Gomez <camilogomezmusic@gmail.com>
2020-08-19 17:59:52 +0000

    Modified to protect against sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,9 @@ const pool = new Pool( {
 });
 
 
+const cohortName = process.argv[2];
+
+const values = [`%${cohortName}%`];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,9 +18,9 @@ FROM assistance_requests
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 JOIN teachers ON teacher_id = teachers.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
